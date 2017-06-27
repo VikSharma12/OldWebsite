ccc1811d15423cedb77e96a0e39cba88d05b39e3
Vik Sharma <shar6770@mylaurier.ca>
2017-06-27 01:11:24 -0400

    Update pictures and footer

--- app.js
+++ app.js
@@ -42,27 +42,7 @@ var main = function(){
     currentDot.removeClass('active-dot');
     prevDot.addClass('active-dot');
   });
-
-  $("#email").mouseover(function(){
-    $(this).animate({width: "80px"}, 'slow');
-  });
-  $("#email").mouseout(function(){
-    $(this).animate({width: "60px"}, 'slow');
-  });
-
-  $("#linkedin").mouseover(function(){
-    $(this).animate({width: "100px"}, 'slow');
-  });
-  $("#linkedin").mouseout(function(){
-    $(this).animate({width: "75px"}, 'slow');
-  });
-
-  $("#github").mouseover(function(){
-    $(this).animate({width: "100px"}, 'slow');
-  });
-  $("#github").mouseout(function(){
-    $(this).animate({width: "75px"}, 'slow');
-  });    
+  
 };
 
 $(document).ready(main);
